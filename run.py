6e6af040d26128cec234e60879256fd5794a7840
zbynekstara <zbynek.stara.internet@hotmail.com>
2015-04-11 21:03:06 +0400

    port

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 # Run a test server.
 from app import app
-app.run(host='127.0.0.1', port=5000, debug=True)
+import os
+
+port = int(os.environ.get('PORT', 5000))
+
+app.run(host='0.0.0.0', port=port, debug=True)
