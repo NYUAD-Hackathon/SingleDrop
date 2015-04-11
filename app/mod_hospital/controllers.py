d5817e4d30cbea870ae3f7fef6af840530032ebf
zbynekstara <zbynek.stara.internet@hotmail.com>
2015-04-11 19:51:28 +0400

    merges

--- app/mod_hospital/controllers.py
+++ app/mod_hospital/controllers.py
@@ -1,6 +1,6 @@
 # Import flask dependencies
 from flask import Blueprint, request, render_template, \
-                  flash, g, session, redirect, url_for
+				  flash, g, session, redirect, url_for
 
 # Import password / encryption helper tools
 from werkzeug import check_password_hash, generate_password_hash
@@ -25,74 +25,74 @@ mod_hospital = Blueprint('hospital', __name__, url_prefix='/hospital')
 # Set the route and accepted methods
 @mod_hospital.route('/manage/')
 def manage():
-    hospital = Hospital.query.filter_by(name="Red Crescent Hospital")
-    return render_template("hospital.html")
+	hospital = Hospital.query.filter_by(name="Red Crescent Hospital")
+	return render_template("hospital.html")
 
 @mod_hospital.route('/login/', methods=['GET', 'POST'])
 def login():
 
-    # If login form is submitted
-    form = LoginForm(request.form)
+	# If login form is submitted
+	form = LoginForm(request.form)
 
-    # Verify the sign in form
-    print form
-    print form.errors
-    if form.validate_on_submit():
+	# Verify the sign in form
+	print form
+	print form.errors
+	if form.validate_on_submit():
 
-        hospital = Hospital.query.filter_by(name=form.name.data).first()
-        print "PASSWORD:", form.password.data
-        print "USER PASS:", hospital.password
-        if hospital and hospital.password == form.password.data:
+		hospital = Hospital.query.filter_by(name=form.name.data).first()
+		print "PASSWORD:", form.password.data
+		print "USER PASS:", hospital.password
+		if hospital and hospital.password == form.password.data:
 
-            session['hospital_id'] = hospital.id
+			session['hospital_id'] = hospital.id
 
-            flash('Welcome %s' % hospital.name)
-            print hospital
-            return redirect("hospital/manage")
+			flash('Welcome %s' % hospital.name)
+			print hospital
+			return redirect("hospital/manage")
 
-    flash('Wrong name or password', 'error-message')
-    print "wrong form"
-    return render_template("hospital/login.html", form=form)
+	flash('Wrong name or password', 'error-message')
+	print "wrong form"
+	return render_template("hospital/login.html", form=form)
 
-@mod_auth.route('/logout/', methods=['GET', 'POST'])
+@mod_hospital.route('/logout/', methods=['GET', 'POST'])
 def logout():
 
-    session.pop('logged_in', None)
-    flash('You were logged out')
+	session.pop('logged_in', None)
+	flash('You were logged out')
 
-    return redirect(url_for('index'))
+	return redirect(url_for('index'))
 
 # Set the route and accepted methods
 @mod_hospital.route('/registration/', methods=['GET', 'POST'])
 def registration():
 	geolocator = Nominatim()
 
-    form = RegistrationForm(request.form)
+	form = RegistrationForm(request.form)
 
-    # Verify the sign in form
-    print form
-    print form.errors
-    if form.validate_on_submit():
+	# Verify the sign in form
+	print form
+	print form.errors
+	if form.validate_on_submit():
 
-        name = form.name.data
-        password = form.password.data
-        city = form.city.data
-        neighborhood = form.neighborhood.data
+		name = form.name.data
+		password = form.password.data
+		city = form.city.data
+		neighborhood = form.neighborhood.data
 
-        address = neighborhood + ", " + city
-        location = geolocator.geocode(address)
+		address = neighborhood + ", " + city
+		location = geolocator.geocode(address)
 
-        lat = location.latitude
-        lon = location.longitude
+		lat = location.latitude
+		lon = location.longitude
 
-        hospital = Hospital(name, password, city, neighborhood, lat, lon)
+		hospital = Hospital(name, password, city, neighborhood, lat, lon)
 
-        db.session.add(hospital)
-        db.session.commit()
-        print "hospital created"
-        print hospital
+		db.session.add(hospital)
+		db.session.commit()
+		print "hospital created"
+		print hospital
 
-        return redirect("hospital/manage")
-    print form.errors
-    print "wrong form"
-    return render_template("hospital/registration.html", form=form)
+		return redirect("hospital/manage")
+	print form.errors
+	print "wrong form"
+	return render_template("hospital/registration.html", form=form)
